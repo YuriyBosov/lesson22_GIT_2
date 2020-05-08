c912dd146b2068f72dab010b096f2adcf2570329
Roman Berezin <romanberezinua@gmail.com>
2020-05-08 21:29:35 +0300

    Added Button to ViewController

--- lesson22_GIT_2/ViewController.swift
+++ lesson22_GIT_2/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var testButton: UIButton?
+    
+    @IBAction func testButtonPressed() {
+        print("Pressed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
